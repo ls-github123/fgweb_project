79d0697f0da1f67e25cb23515cc7fa8bab8e08d8
李硕 <lpsserver443@outlook.com>
2024-09-01 22:20:33 +0800

    改进celery报错提示校验代码

--- fgweb_project_api/celery_module/sms/tasks.py
+++ fgweb_project_api/celery_module/sms/tasks.py
@@ -4,6 +4,8 @@ from fgweb_project_api.settings.utils.aliyun_sms_server import send_sms
 # 从main中导入celery对象 
 @app.task(name="send_message")
 def send_message(phone_number, template_param):
-    print('--验证码短信发送任务被执行--')
-    res = send_sms(phone_number, template_param)
-    return res
+    try:
+        # 发送短信的代码
+        return {'status': 'success', 'message': '短信发送成功'}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
