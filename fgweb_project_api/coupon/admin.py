bdd3f0fe8ec0164ca8689827ab7d66bf93991a87
李硕 <lpsserver443@outlook.com>
2024-09-09 17:23:35 +0800

    优惠券代码调整

--- fgweb_project_api/coupon/admin.py
+++ fgweb_project_api/coupon/admin.py
@@ -1,99 +1,101 @@
-from typing import Any
 from django.contrib import admin
-from . import models
+from coupon.models import CouponModels,CouponCourseDirectionModel,CouponCourseCategoryModel,CouponCourseModel,CouponLogModel
 from django_redis import get_redis_connection
 import json
+# Register your models here.
 
-# 课程专业/方向
-# 外键关联类的管理站点内嵌显示
-class CouponDiretionInLine(admin.TabularInline):
-    model = models.CouponCourseDirectionModel
-    fields = ['id', 'direction']
-    
-# 课程分类
-# 外键关联类的管理站点内嵌显示
-class CouponCategoryInLine(admin.TabularInline):
-    model = models.CouponCourseCategoryModel
-    fields = ['id', 'category']
-    
-# 课程信息
-class CouponCourseInLine(admin.TabularInline):
-    model = models.CouponCourseModel
-    fields = ['id', 'coupon', 'course']
-    
-# 优惠券
-class CouponModelsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'start_time', 'end_time',
-                    'total', 'has_total', 'coupon_type', 'get_type']
-    inlines = [CouponDiretionInLine, CouponCategoryInLine, CouponCourseInLine]
-admin.site.register(models.CouponModels, CouponModelsAdmin)
 
-# 日志/发送管理
-class CouponLogModelsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'coupon', 'use_time', 'use_status']
+# 方向
+# 外键关联显示
+class CouponDirectionInline(admin.TabularInline):
+    model = CouponCourseDirectionModel
+    fields = ['id','direction']
+
+
+class CouponCategoryInline(admin.TabularInline):
+    model = CouponCourseCategoryModel
+    fields = ['id','category']
+
+class CouponCourseInline(admin.TabularInline):
+    model = CouponCourseModel
+    fields = ['id','course']
+
+
+# 优惠券自定义类
+class CouponModelAdmin(admin.ModelAdmin):
+    list_display = ['id','name','start_time','end_time'
+                    ,'total','has_total','coupon_type','get_type']
+    # 实现外键关联的
+    inlines = [CouponDirectionInline,CouponCategoryInline,CouponCourseInline]
+admin.site.register(CouponModels,CouponModelAdmin)
+
+# admin.site.register(CouponCourseDirectionModel)
+# admin.site.register(CouponCourseCategoryModel)
+# admin.site.register(CouponCourseModel)
+
+
+class CouponLogModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','coupon','use_time','use_status']
     def save_model(self, request, obj, form, change):
-        # 管理站点中点击保存，触发的自动执行的函数，钩子
-        # request:本次客户端提交的请求对象
-        # obj：本次操作的模型类实例对象
-        # form:本次客户端提交的表单数据
-        # change：值为True，表示更新数据，值为false：添加/新增数据
-        # Add update
+        # 管理站点点击保存，自动触发的函数，钩子函数
+        # request: 本次客户端提交的请求对象
+        # obj : 本次操作的模型类实例对象
+        # form:提交的表单数据
+        # change: true 表示更新数据，  false 添加新增数据
         obj.save()
-        # 同步记录到redis中
         redis = get_redis_connection('coupon')
         if obj.use_status == 0 and obj.use_time == None:
             pipe = redis.pipeline()
             pipe.multi()
-            # 首先存储数据结构 构建
-            # 优惠券为一个对象 属性较多
+            # 往reids中，存数据，
+            # 构建数据存储类型，属性全部放如reids中
             coupon_dict = {
-                "coupon_id": obj.coupon.id,
-                "name": obj.coupon.name,
-                "discount": obj.coupon.discount,
-                "get_discount_display": obj.coupon.get_discount_display(),
-                "coupon_type": obj.coupon.coupon_type,
-                "get_coupon_type_display": obj.coupon.get_coupon_type_display(),
-                "start_time": obj.coupon.start_time.strftime("%Y-%m-%d %H:%M:%S"),
-                "end_time": obj.coupon.end_time.strftime("%Y-%m-%d %H:%M:%S"),
-                "get_type": obj.coupon.get_type,
-                "get_get_type_display": obj.coupon.get_get_type_display(),
-                "condition": obj.coupon.condition,
-                "sale": obj.coupon.sale,
-                "to_diretion": json.dumps(list(obj.coupon.to_diretion.values("diretion__id", "diretion__name"))),
-                "to_category": json.dumps(list(obj.coupon.to_category.values("category__id", "category__name"))),
-                "to_course": json.dumps(list(obj.coupon.to_course.values("course__id", "course__name"))),
+                "coupon_id":obj.coupon.id,
+                "name":obj.coupon.name,
+                "discount":obj.coupon.discount,
+                "get_discount_display":obj.coupon.get_discount_display(),
+                "coupon_type":obj.coupon.coupon_type,
+                "get_coupon_type_display":obj.coupon.get_coupon_type_display(),
+                "get_type":obj.coupon.get_type,
+                "get_get_type_display":obj.coupon.get_get_type_display(),
+                "start_time":obj.coupon.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                "end_time":obj.coupon.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                "condition":obj.coupon.condition,
+                "sale":obj.coupon.sale,
+                "to_direction":json.dumps(list(obj.coupon.to_direction.values('direction__id','direction__name'))),
+                "to_category":json.dumps(list(obj.coupon.to_category.values('category__id','category__name'))),
+                "to_course":json.dumps(list(obj.coupon.to_course.values('course__id','course__name'))),
+                # json     list
             }
-            # 专业、分类、课程
-            print(f"添加的用户ID:{str(obj.user.id)}")
-            pipe.hset(f"{obj.user.id}:{obj.id}", mapping = coupon_dict)
-            print(f"打印redis存储的Key:{obj.user.id}:{obj.id}")
-            
-            # 过期时间
-            # setex key 100 value
+            # 用户id
+            # obj.user.id
+            print('用户id:',obj.user.id)
+
+            # pipe.hset(f"{obj.user.id}:{obj.id}","coupon_id",obj.coupon.id)
+            # pipe.hset(f"{obj.user.id}:{obj.id}","name",obj.coupon.name)
+
+            pipe.hset(f"{obj.user.id}:{obj.id}",mapping=coupon_dict)
+            print("打印存入redis中的key")
+            print(f"{obj.user.id}:{obj.id}")
+
+            # 设置过期时间
             from datetime import datetime
-            # redis: 给任意类型的key设置过期时间
-            # 设置当前用户优惠券有效期
-            pipe.expire(f"{obj.user.id}:{obj.id}", int(obj.coupon.ent_time.timestamp()-datetime.now().timestamp()))
+            # 数据类型，都可以过期时间，，根据key设置
+            pipe.expire(f"{obj.user.id}:{obj.id}",int(obj.coupon.end_time.timestamp()-datetime.now().timestamp()))
             pipe.execute()
         else:
             redis.delete(f"{obj.user.id}:{obj.id}")
-    
+
     def delete_model(self, request, obj):
-        """删除记录时候自动执行的钩子函数"""
-        # 管理站点中删除当前优惠券，redis中也需要删除
-        redis = get_redis_connection("coupon")
+        redis = get_redis_connection('coupon')
         redis.delete(f"{obj.user.id}:{obj.id}")
         obj.delete()
-    
-    # 数据导入，站点管理、获取优惠券、过滤有效优惠券
+
     def delete_queryset(self, request, queryset):
-        # 列表页面中进行删除记录，选择可能是多条数据
         redis = get_redis_connection("coupon")
-        # 遍历集合queryset
         for qs in queryset:
             redis.delete(f"{qs.user.id}:{qs.id}")
         queryset.delete()
 
-admin.site.register(models.CouponLogModel, CouponLogModelsAdmin)
 
-# 优惠券在数据库存储中，也可以在reids存储一份
+admin.site.register(CouponLogModel,CouponLogModelAdmin)
--- fgweb_project_api/coupon/services.py
+++ fgweb_project_api/coupon/services.py
@@ -1,61 +1,60 @@
-from django_redis import get_redis_connection
 import json
+
+from django_redis import get_redis_connection
 from course.models import CourseModel
 
-# 获取当前用户所有优惠券
-# 有效/无效
+
+
+# 获取当前用户的所有优惠券：有效、无效
 def get_user_coupon_list(user_id):
     # f"userid:coupon_id"
     redis = get_redis_connection('coupon')
-    # 匹配获取当前redis中以user_id 开头的所有key
+    # 匹配获取当前reids中以user_id： 开头的所有的key
     coupon_list = redis.keys(f"{user_id}:*")
-    coupon_id_list = [item for item in coupon_list]
-    
-    # 构建空列表 用于存储循环后的优惠券数据
+    # b'1：9   1：10  1：11
+    coupon_id_list = [item.decode() for item in coupon_list]
+    # 构建空列表，用于存储，循环后的优惠券数据
     coupon_data = []
     for coupon_key in coupon_id_list:
-        # 获取优惠券ID 默认生成字段
+        # 获取优惠券id,默认生成一个字段
         coupon_item = {"user_coupon_id":int(coupon_key.split(":")[-1])}
-        # 根据key 获取对应所有属性
+        # 根据key，获取对应所有属性
         coupon_hash = redis.hgetall(coupon_key)
-        for key, value in coupon_hash.items():
+        for key,value in coupon_hash.items():
             key = key.decode()
             value = value.decode()
-            # 如果key为外键，外键存放json格式数据
-            # 需要将字符串转换python中的字典格式数据类型
-            if key in ['to_direction', 'to_category', 'to_course']:
-                # json.loads()方法 
-                # 用于将符合JSON标准的字符串转换为Python数据结构
-                #（常见的是转换为字典或列表）
+            # 如果key是外键，因为外键存放的json格式的数据，所以需要将字符串转换python中的字典格式数据类型
+            if key in ['to_direction','to_category','to_course']:
                 value = json.loads(value)
+
             coupon_item[key] = value
         coupon_data.append(coupon_item)
     return coupon_data
-# [{"coupon_id":1,"name":'7天学费...'}，{"coupon_id":1,"name":'7天学费...'}]
+    # [{"coupon_id":1,"name":'7天学费...'}，{"coupon_id":1,"name":'7天学费...'}]
 
 
 def get_user_enable_coupon_list(user_id):
-    # 获取指定用户当次下单的优惠券列表
-    # 1.首先获取购物车中选中的商品
+    # 获取指定用户当此下单的优惠券列表
+    # 1、获取购物车中，选中的商品
     redis = get_redis_connection('cart')
-    # 获取购物车中所有选中的商品数据
+    # 获取购物车中所有数据------选中
     cart_hash = redis.hgetall(f"cart_{user_id}")
-    
+
     # cart_1 1 0/1
     # 获取选中
-    course_id_list = {int(key.decode()) for key, value in cart_hash.items() if value ==b'1'}
-    # 根据课程id列表 获取对应课程
-    course_list = CourseModel.objects.filter(id__in = course_id_list, is_deleted = False, is_show = True)
-    
-    # 通过课程列表 获取分类 id列表 方向id列表
+    course_id_list ={int(key.decode()) for key,value in cart_hash.items() if value == b'1'}
+    # 根据课程id列表，获取对应的课程
+    coruse_list = CourseModel.objects.filter(id__in=course_id_list,is_deleted=False,is_show=True)
+
+    # 通过课程列表，获取分类id列表，方向id列表
     direction_id_list = set()
     category_id_list = set()
-    
-    for course in course_list:
+
+    for course in coruse_list:
         direction_id_list.add(int(course.direction.id))
         category_id_list.add(int(course.category.id))
-        
-    # 获取优惠券列表
+
+    # 优惠券列表获取到
     coupon_data = get_user_coupon_list(user_id)
     # 构建一个可用的优惠列表对象
     enable_coupon_list = []
@@ -68,25 +67,31 @@ def get_user_enable_coupon_list(user_id):
         elif coupon_type == 1:
             # 方向优惠券
             coupon_direction = {direction_item['direction__id'] for direction_item in item.get("to_direction")}
+            print('打印优惠券对应的方向id列表')
+            print(coupon_direction)
             # 交集
+            # list2 = list1 & list2
             ret = coupon_direction & direction_id_list
-            item['enable_course'] = { int(course.id) for course in course_list if course.direction.id in ret }
-            enable_coupon_list.append(item)
+            if len(ret)>0:
+                item['enable_course'] = {int(course.id) for course in coruse_list if course.direction.id in ret }
+                enable_coupon_list.append(item)
         elif coupon_type == 2:
-            # 分类
+            # 分类优惠券
             coupon_category = {category_item['category__id'] for category_item in item.get("to_category")}
             print('打印优惠券对应的分类id列表')
             print(coupon_category)
-            
+
             ret = coupon_category & category_id_list
-            if len(ret) > 0:
-                item['enable_course'] = {int(course.id) for course in course_list if course.category.id in ret}
+
+            if len(ret)>0:
+                item['enable_course'] = {int(course.id) for course in coruse_list if course.category.id in ret}
                 enable_coupon_list.append(item)
         elif coupon_type == 3:
-            coupon_course = {course_item['coures_id'] for course_item in item.get("to_course")}
+            coupon_course = {course_item['course__id'] for course_item in item.get("to_course")}
+
             ret = coupon_course & course_id_list
             if len(ret) > 0:
                 # 遍历所有课程列表
-                item['enable_course'] = {int(course.id) for course in course_list if course.id in ret}
+                item['enable_course'] = {int(course.id) for course in coruse_list if course.id in ret}
                 enable_coupon_list.append(item)
     return enable_coupon_list
